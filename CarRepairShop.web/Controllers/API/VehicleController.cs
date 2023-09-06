065d0d273279f1af45778f7c3741425891f72bc5
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-09-06 14:31:12 +0100

    API vehicles controller

---
+++ CarRepairShop.web/Controllers/API/VehicleController.cs
@@ -0,0 +1,23 @@
+﻿using CarRepairShop.web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRepairShop.web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : Controller
+    {
+        private readonly IVehicleRepository _vehicleRepository;
+
+        public VehiclesController(IVehicleRepository vehicleRepository)
+        {
+            _vehicleRepository = vehicleRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetVeicles()
+        {
+            return Ok(_vehicleRepository.GetAll());
+        }
+    }
+}
