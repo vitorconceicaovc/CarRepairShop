7c38d47b2c42bbddf6248bc0b7bf19ae8e4c8a09
Vítor Conceição <vitoralmeidavc@outlook.pt>
2023-10-04 16:19:51 +0100

    Users index

---
+++ CarRepairShop.web/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using CarRepairShop.web.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Linq;
+
+namespace CarRepairShop.web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(
+            DataContext context
+            )
+        {
+            _context = context;
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        // GET: Users
+        public IActionResult Index()
+        {
+            //return View(_vehicleRepository.GetAll().OrderBy(b => b.CarPlate));
+            var users = _context.Users.ToList(); // Obter todos os usuários e converter em uma lista
+            return View(users);
+        }
+    }
+}
